24cac8dac5b20883c9a4ce77fd94930bb3309c89
alih6051 <alih6051@gmail.com>
2022-05-26 00:39:08 +0530

    local storage feature added

--- scripts/app.js
+++ scripts/app.js
@@ -75,4 +75,13 @@ cityForm.addEventListener('submit', e => {
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 
+    //setting local storage
+    localStorage.setItem('city', city);
+
 });
+
+if (localStorage.getItem('city')) {
+    updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+}
